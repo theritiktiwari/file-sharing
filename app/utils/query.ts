b41e9bfebe9398ecfa79c804088aec02f761b4ab
Ritik Tiwari <ritiktiwariwork@gmail.com>
2024-02-26 21:09:32 +0530

    Added some queries

---
+++ app/utils/query.ts
@@ -0,0 +1,83 @@
+"use server";
+import prisma from "@/app/utils/db";
+
+// Save the file to the database
+export async function saveFileToDB(userId: any, key: string) {
+    const file = await prisma.file?.create({
+        data: {
+            userId,
+            key
+        }
+    });
+    return file;
+}
+
+// User can get all the files that he has uploaded
+export async function getAllFiles(userId: string) {
+    const files = await prisma.file?.findMany({
+        where: { userId }
+    });
+    return files;
+}
+
+// get the single file by id
+export async function getFileById(id: string) {
+    const file = await prisma.file?.findUnique({
+        where: { id }
+    });
+    return file;
+}
+
+// get user details by email
+export async function getUserByEmail(email: string) {
+    const user = await prisma.user?.findUnique({
+        where: { email }
+    });
+    return user;
+}
+
+// update the user profile
+export async function updateUserProfile(email: string, name?: string, image?: string) {
+    const user = await prisma.user?.update({
+        where: { email: email },
+        data: { name, image }
+    });
+    return user;
+}
+
+// get all the sessions of the user
+export async function getAllSessions(userId: string) {
+    const sessions = await prisma.session?.findMany({
+        where: { userId }
+    });
+    return sessions;
+}
+
+// remove all the sessions of the user
+export async function removeAllSessions(userId: string) {
+    try {
+        const sessions = await prisma.session?.deleteMany({
+            where: { userId }
+        });
+        return sessions;
+    } catch (e: any) {
+        return {
+            type: "error",
+            message: e.message
+        }
+    }
+}
+
+// Admin can get all the users
+export async function getAllUsers() {
+    const users = await prisma.user?.findMany();
+    return users;
+}
+
+// Admin can delete the user by id
+export async function deleteUserById(id: string) {
+    const user = await prisma.user?.delete({
+        where: { id }
+    });
+    return user;
+}
