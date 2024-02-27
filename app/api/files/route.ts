9cc965648561d9a47f50a6596d46687b7aee1654
Ritik Tiwari <ritiktiwariwork@gmail.com>
2024-02-27 23:32:07 +0530

    Fix the api route for uploading file

--- app/api/files/route.ts
+++ app/api/files/route.ts
@@ -2,19 +2,13 @@ import { NextRequest, NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 import pinataSDK from "@pinata/sdk";
 import { Readable } from "stream";
-// import { execSync } from "child_process";
+import { getUserByEmail, saveFileToDB } from "@/app/utils/query";
 const pinata = new pinataSDK({ pinataJWTKey: process.env.PINATA_JWT });
 
-// const generateSecretCode = () => {
-//     const randomString = execSync('openssl rand -hex 32').toString();
-//     return randomString;
-// };
-
-// export const config = {
-//     api: {
-//         bodyParser: false,
-//     },
-// };
+const generateSecretCode = () => {
+    const randomString = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    return randomString;
+}
 
 const saveFile = async (file: any) => {
     try {
@@ -33,10 +27,29 @@ const saveFile = async (file: any) => {
     }
 };
 
+// const saveToContract = async (id: string, jwt: string) => {
+//     try {
+//         const contractAddress = process.env.CONTRACT_ADDRESS as string;
+
+//         const privateKey = process.env.WALLET_PRIVATE_KEY as string;
+//         const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL as string);
+
+//         const wallet = new ethers.Wallet(privateKey, provider);
+//         const contract = new ethers.Contract(contractAddress, contractABI, wallet);
+
+//         const tx = await contract.uploadData(id, jwt);
+//         await tx.wait();
+
+//         return true;
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+
 export async function POST(request: NextRequest, res: any) {
     try {
         const formData = await request.formData();
-        
+
         const file = formData.get("file") as Blob | null;
         if (!file) {
             return NextResponse.json({
@@ -53,7 +66,7 @@ export async function POST(request: NextRequest, res: any) {
             }, { status: 400 });
         }
 
-        // const userToken = generateSecretCode();
+        const userToken = generateSecretCode();
         const saveData = await saveFile(file);
 
         if (!saveData?.IpfsHash) {
@@ -63,23 +76,19 @@ export async function POST(request: NextRequest, res: any) {
             }, { status: 500 });
         }
 
-        const jwt_secret = process.env.JWT_SECRET;
-        const jwtToken = saveData?.IpfsHash && jwt_secret && jwt.sign({ hash: saveData?.IpfsHash, emails }, jwt_secret);
+        const jwtToken = saveData?.IpfsHash && jwt.sign({ hash: saveData?.IpfsHash, emails }, process.env.JWT_SECRET as string);
 
-        if (jwtToken) {
-            // add the data to smart contract
-        }
-
-        const finalData = {
-            ...saveData,
-            jwtToken,
-            emails,
+        if (jwtToken && userToken) {
+            // add data to the database
+            const senderEmail = emails[emails.length - 1];
+            const user = await getUserByEmail(senderEmail);
+            await saveFileToDB(user?.id as string, userToken, jwtToken);
         }
 
         return NextResponse.json({
             type: "success",
             message: "File uploaded successfully.",
-            data: finalData
+            data: { userToken }
         }, { status: 200 });
     } catch (error: any) {
         return NextResponse.json({
